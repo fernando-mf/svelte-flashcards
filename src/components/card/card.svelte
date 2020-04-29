d5898f7b7a219349ef044efd6eb449d19d675c03
fernando-mf <fernando.1703@hotmail.com>
2020-04-29 01:58:36 -0400

    Project setup

---
+++ src/components/card/card.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  .card {
+    border: 1px solid black;
+    border-radius: 30px;
+  }
+</style>
+
+<div class="card">
+  <h1>{name}</h1>
+  <p>This is the card component</p>
+</div>
