3b8172e3d9b56e7923ed513ead71eda253149ca5
fernando-mf <fernando.1703@hotmail.com>
2020-06-29 02:00:04 -0400

    Use snowpack

---
+++ src/components/Card.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let title;
+  export let content;
+</script>
+
+<div class="border max-w-md rounded-lg">
+  <div class="text-lg font-bold">{title}</div>
+  <div>{content}</div>
+</div>
